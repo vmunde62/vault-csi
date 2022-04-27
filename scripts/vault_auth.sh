4ff36313516969423ec5465575e55ceca76ab39d
Vishal <vmunde62@gmail.com>
2022-04-27 18:08:15 +0530

    added vault_auth.sh

--- scripts/vault_auth.sh
+++ scripts/vault_auth.sh
@@ -1,5 +1,3 @@
-vault write auth/${params.cluster}/config \
-    token_reviewer_jwt="${env.TOKEN_REVIEW_JWT}" \
-    kubernetes_host="${env.KUBE_HOST}" \
-    kubernetes_ca_cert="${env.KUBE_CA_CERT}" \
-    issuer="https://kubernetes.default.svc.cluster.local"
+echo "${env.TOKEN_REVIEW_JWT}"
+echo "${env.KUBE_CA_CERT}"
+echo "${env.KUBE_HOST}"
