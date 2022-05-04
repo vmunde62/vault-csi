9806c0ec2b8f235812c35b9f8d245a4c4533a311
Vishal <vmunde62@gmail.com>
2022-05-04 18:44:00 +0530

    added values script

--- scripts/set_valuesfile.sh
+++ scripts/set_valuesfile.sh
@@ -1,8 +1,8 @@
-if [ $params.clusterName = cluster1 ];then
+if [ $clusterName = cluster1 ];then
 values_file='c1values.yaml';
-elif [ $params.clusterName = cluster2 ];then
+elif [ $clusterName = cluster2 ];then
 values_file='c2values.yaml';
-elif [ $params.clusterName = cluster3 ];then
+elif [ $clusterName = cluster3 ];then
 values_file='c3values.yaml';
 fi
 
